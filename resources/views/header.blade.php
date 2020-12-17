0982379d46cacc4c3b133b3849c938e0dc9f0192
ayoubben5 <60117038+ayoubben5@users.noreply.github.com>
2020-12-17 20:28:52 +0100

    cart feature done

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,3 +1,7 @@
+<?php 
+use App\Http\Controllers\ProductController;
+$total= ProductController::cartItem();
+?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,7 +20,7 @@
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form>
         <li class="nav-item ">
-          <a class="nav-link " href="#">Cart</a>
+          <a class="nav-link " href="#">Cart({{$total}})</a>
         </li>
       </ul>
     </div>
